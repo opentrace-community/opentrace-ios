3dd9305638d93065f8220551c9cfe3b7fb0e1c6e
Neil Horton <neil.horton@santander.co.uk>
2020-04-15 11:05:36 +0100

    remove otp from onboarding first pass through

--- OpenTrace/Onboarding/View Controllers/HowItWorksViewController.swift
+++ OpenTrace/Onboarding/View Controllers/HowItWorksViewController.swift
@@ -10,12 +10,7 @@ class HowItWorksViewController: UIViewController {
     @IBAction func greatBtnOnClick(_ sender: UIButton) {
 
         OnboardingManager.shared.completedIWantToHelp = true
-
-        if Auth.auth().currentUser == nil {
-            self.performSegue(withIdentifier: "iWantToHelpToPhoneSegue", sender: self)
-        } else {
-            self.performSegue(withIdentifier: "iWantToHelpToConsentSegue", sender: self)
-        }
+        self.performSegue(withIdentifier: "iWantToHelpToConsentSegue", sender: self)
     }
 
     override func viewDidLoad() {
