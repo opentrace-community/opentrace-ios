408a0cff9e7a17495c960c772f973a82f97e81d8
Sam Dods <sam@theappbusiness.com>
2020-04-16 16:52:35 +0100

    added app icon

---
+++ OpenTrace/Monitoring/Symptoms/DecisionManager/DecisionManager.swift
@@ -0,0 +1,13 @@
+//
+//  DecisionManager.swift
+//  OpenTrace
+//
+//  Created by Sam Dods on 16/04/2020.
+//  Copyright © 2020 OpenTrace. All rights reserved.
+//
+
+import Foundation
+
+class DecisionManager {
+    
+}
